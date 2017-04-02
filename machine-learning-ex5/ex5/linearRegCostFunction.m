dc2ef140d640f806e6ee6fdb593eee5e4a281c8a
hedgehog-millenium <mac@MACs-MacBook-Air.local>
2017-04-02 23:13:30 +0400

    ex5 SCORE-25

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,14 +19,12 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+H = X*theta ;
+theta1 = [0 ; theta(2:end, :)];
+regPart = lambda/(2*m) * sum((theta1.^2)) ;
+sumSquerPart = sum((H-y).^2) ;
 
-
-
-
-
-
-
-
+J = 1/(2*m)*sumSquerPart+regPart;
 
 
 
