73969de1c5e6a1b8f7476f4822c19399f3d6caea
hedgehog-millenium <mac@MACs-MacBook-Air.local>
2017-04-24 12:53:43 +0400

    ex8 score-100

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,18 +40,10 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = 0.5*sum(((X*Theta'-Y).^2)(R==1))+0.5*lambda*( sum(sum(Theta.^2))+sum(sum(X.^2)) );
 
-
-
-
-
-
-
-
-
-
-
-
+X_grad = ((X*Theta'-Y).*R)*Theta+lambda*X;
+Theta_grad = ((X*Theta'-Y).*R)'*X+lambda*Theta;
 
 
 
